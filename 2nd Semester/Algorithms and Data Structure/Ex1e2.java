ee9d6c6839d3c558f50b0a7a078db841f5856f23
Gonçalo Santos <72096290+GongasSantos15@users.noreply.github.com>
2024-02-20 15:36:40 +0000

    Update Ex1e2.java

--- 2nd Semester/Algorithms and Data Structure/Ex1e2.java
+++ 2nd Semester/Algorithms and Data Structure/Ex1e2.java
@@ -1,10 +1,15 @@
 package ficha1;
 
-public class Ex1e2 {
+public class Ex123 {
 
 	public static void main(String[] args) {
 		int[] vetorOrdenado = criarVetorOrdenado(25);
+		System.out.println("Exercício 2:");
 		imprimirVetor(vetorOrdenado);
+		
+		int[] vetorEmbaralhado = embaralharVetor(3, vetorOrdenado);
+		System.out.println("\nExercício 3:");
+		imprimirVetor(vetorEmbaralhado);
 	}
 	
 	// Exercício 1
@@ -27,6 +32,25 @@ public class Ex1e2 {
 					System.out.println("...");
 				}
 			}
+		}
+		
+	// Exercício 3
+		static int[] embaralharVetor(int nTrocas, int[] vetor) {
+			for (int i = 0; i < nTrocas; i++) {
+				
+				// Criação de 2 variáveis para os 2 índices a trocar do array
+					int index1 = (int) (Math.random() * vetor.length);
+					int index2 = (int) (Math.random() * vetor.length);
+				
+				// Guardar a posição das 2 variáveis criadas anteriormente
+					int auxVetor1 = vetor[index1];
+					int auxVetor2 = vetor[index2];
+				
+				// Trocar as 2 posições
+					vetor[index1] = auxVetor2;
+					vetor[index2] = auxVetor1;
+			}
+			return vetor;
 		}
 
 }
