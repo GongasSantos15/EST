1b6ef6e605e4c7a6e2a511ee7c2085538aafaac2
Gonçalo Santos <72096290+GongasSantos15@users.noreply.github.com>
2024-02-27 15:30:00 +0000

    Update Ex1e2.java

--- 2nd Semester/Algorithms and Data Structure/Ex1e2.java
+++ 2nd Semester/Algorithms and Data Structure/Ex1e2.java
@@ -10,6 +10,35 @@ public class Ex123 {
 		int[] vetorEmbaralhado = embaralharVetor(3, vetorOrdenado);
 		System.out.println("\nExercício 3:");
 		imprimirVetor(vetorEmbaralhado);
+
+//		System.out.printf("\nExercício 4a:\n A soma de todos os elementos do array é: %d\n", somaTodosElementos(vetorOrdenado));
+//		
+//		System.out.printf("\nExercício 4b:\n O maior índice do array é: %d\n", indiceMaiorElemento(vetorEmbaralhado));
+//		
+//		System.out.printf("\nExercício 4c:\n O maior índice do array é: %d\n", indiceMenorElemento(vetorEmbaralhado));
+		
+		// Procura Linear
+			int[] vetorOrdenado = criarVetorOrdenado(20);
+			int[] vetorEmbaralhado = embaralharVetor(50, vetorOrdenado);
+			imprimirVetor(vetorEmbaralhado);
+			
+			System.out.printf("\nIndique o elemento a procurar: ");
+			Scanner input = new Scanner(System.in);
+			int valorUser = input.nextInt();
+			
+			System.out.printf("O valor introduzido está na posição %d", procuraLinear(vetorEmbaralhado, valorUser));
+			
+			
+		// Procura Binária
+			vetorOrdenado = criarVetorOrdenado(20);
+			imprimirVetor(vetorOrdenado);
+			
+			System.out.println("\nIndique o elemento a procurar: ");
+			input = new Scanner(System.in);
+			valorUser = input.nextInt();
+			
+			System.out.printf("O valor introduzido está na posição %d", procuraBinaria(vetorOrdenado, valorUser));
+			input.close();
 	}
 	
 	// Exercício 1
@@ -52,5 +81,78 @@ public class Ex123 {
 			}
 			return vetor;
 		}
+	// Exercício 4
+		// Soma de todos os elementos de um array
+			static int somaTodosElementos(int[] vetor) {
+					
+				int soma = 0;
+					
+				for (int i = 0; i < vetor.length; i++ ) {
+					soma += vetor[i];
+				}
+				return soma;
+			}
+		
+		 // Índice do maior elemento
+			static int indiceMaiorElemento(int[] vetor) {
+				
+				int maiorIndice = 0;
+				
+				if (vetor == null || vetor.length == 0) {
+					return -1;
+				}
+				
+				for (int i = 1; i < vetor.length; i++) {
+					maiorIndice = vetor[i] > vetor[maiorIndice] ? i : maiorIndice;
+				}
+				return maiorIndice;
+			}
+			
+		// Índice do menor elemento
+			static int indiceMenorElemento(int[] vetor) {
+				
+				int menorIndice = 0;
+				
+				for (int i = 1; i < vetor.length; i++) {
+					menorIndice = vetor[i] < vetor[menorIndice] ? i : menorIndice;
+				}
+				return menorIndice;
+			}
+			
+		// Exercício 5
+			static int procuraLinear(int[] vetor, int valor) {
+				for (int i = 0; i < vetor.length; i++) {
+					if (vetor[i] == valor) {
+						return i;
+					}
+				}
+				return -1;
+			}
+			
+			static int procuraBinaria(int[] vetor, int valor) {
+				
+				boolean encontrar = false;
+				int esquerda = 0;
+				int direita = vetor.length-1;
+				int meio = 0;
+				
+				while (esquerda <= direita && !encontrar) {
+						meio = esquerda + direita / 2;
+						
+					if (vetor[meio] == valor) {
+						encontrar = true;
+					} else if (vetor[meio] < valor) {
+						direita = valor - 1;
+						encontrar = false;
+					} else {
+						esquerda = valor + 1;
+						encontrar = false;
+					}
+					return -1;
+				}
+				return meio;
+					
+					
+			}
 
 }
